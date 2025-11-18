47e3a960f4d5801cf1298cc180ae891b78fbaacf
Afzal-cool <afzalahmed12598@gmail.com>
2025-11-18 14:23:27 +0530

    committed

--- app/src/main/java/com/posapp/ui/BillAdapter.java
+++ app/src/main/java/com/posapp/ui/BillAdapter.java
@@ -1,4 +1,9 @@
 package com.posapp.ui;
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.posapp.data.BillItem
+import com.posapp.data.databinding.ItemBillBinding
 public class BillAdapter {
 }
